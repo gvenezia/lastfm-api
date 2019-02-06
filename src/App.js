68b98cfaa24b248245323583095bd145623569b7
Gaetano <g.venezia@outlook.com>
2019-02-06 12:43:05 -0600

    Add lastfm API setup and fetch similar artists to input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+// import logo from './logo.svg';
+// import './App.css';
 
 class App extends Component {
+  state = {entry: ''}
+
+
+
+  getLastFM = (entry) => {
+    // var lastFMinstance = axios.create({
+    //   headers: {'Access-Control-Allow-Origin:': '*'}
+    // });
+
+    // let lastFMurl = `http://www.last.fm/api/auth/?api_key=${process.env.REACT_APP_LASTM_KEY}`;
+
+    axios.get(`http://ws.audioscrobbler.com/2.0/?method=artist.getSimilar&&artist=${entry}&api_key=${process.env.REACT_APP_LASTM_KEY}`)
+      .then( results => {
+        console.log(results);
+      })
+      .catch( err => {
+        console.log(err)
+      });  
+  }
+
+  
+  
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit={event => {
+            event.preventDefault();
+
+            this.getLastFM(this.state.entry);
+          }
+        }>
+          <input 
+                type="text"
+                value={this.state.entry}
+                onChange={ event => this.setState({entry: event.target.value}) } />
+        </form>
       </div>
     );
   }
